bfb80e93bb6414b7d31de181b11fb8d4c649eedb
Ignacio Galindo <joiggama@gmail.com>
2014-07-30 10:59:42 -0500

    User Story 1
    You work for a bank, which has recently purchased an ingenious machine
    to assist in reading letters and faxes sent in by branch offices. The
    machine scans the paper documents, and produces a file with a number of
    entries which each look like this:
    
        _  _     _  _  _  _  _
      | _| _||_||_ |_   ||_||_|
      ||_  _|  | _||_|  ||_| _|
    
    Each entry is 4 lines long, and each line has 27 characters. The first 3
    lines of each entry contain an account number written using pipes and
    underscores, and the fourth line is blank. Each account number should
    have 9 digits, all of which should be in the range 0-9. A normal file
    contains around 500 entries.
    
    Your first task is to write a program that can take this file and parse
    it into actual account numbers.

---
+++ account_numbers/parser.go
@@ -0,0 +1,90 @@
+package account_numbers
+
+import (
+	"fmt"
+	"io/ioutil"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+var dictionary = make(map[int]string, 10)
+
+func ReadFile(fileName string) string {
+	fmt.Println("Reading", fileName)
+	content, err := ioutil.ReadFile(fileName)
+
+	if err != nil {
+		fmt.Println("Error reading", fileName, err.Error())
+	}
+
+	return strings.Trim(string(content), "\n\n\n")
+}
+
+func ParseEntry(entry string) string {
+	var digits string
+
+	for _, digit := range GetDigits(entry, 27) {
+		digits += ParseDigit(digit)
+	}
+
+	return digits
+}
+
+func ParseDigit(digit string) string {
+	var value string
+
+	for number, representation := range dictionary {
+		if representation == digit {
+			value = strconv.Itoa(number)
+		}
+	}
+
+	return value
+}
+
+func GetDigits(entry string, lineLength int) []string {
+	var digits []string
+
+	for index := 0; index < lineLength; index += 3 {
+		digit := ""
+
+		for _, line := range strings.Split(entry, "\n") {
+			if line != "" {
+				digit += line[index:index+3] + "\n"
+			} else {
+				digit += "   \n"
+			}
+		}
+		digits = append(digits, digit)
+	}
+	return digits
+}
+
+func GetEntries(content string) []string {
+	return regexp.MustCompile(`\t*\n\n`).Split(content, -1)
+}
+
+func LoadDictionary() {
+	contents := ReadFile("digits.txt")
+	entry := GetEntries(contents)[0]
+	digits := GetDigits(entry, 30)
+	for i, v := range digits {
+		dictionary[i] = v
+	}
+}
+
+func Parse(fileName string) {
+	LoadDictionary()
+	contents := ReadFile(fileName)
+	entries := GetEntries(contents)
+	fmt.Println("Found", len(entries), "entries in", fileName)
+
+	var accountNumbers []string
+
+	for _, entry := range entries {
+		accountNumbers = append(accountNumbers, ParseEntry(entry))
+	}
+
+	fmt.Println(accountNumbers)
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/joiggama/kata-go-bank-ocr/account_numbers"
+)
+
+func main() {
+	account_numbers.Parse("numbers.txt")
+}
